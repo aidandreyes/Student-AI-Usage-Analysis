2665e94a064de109c7b55afe9478ea5b32bf92b9
Aidan Reyes <aidanreyes2005@gmail.com>
2026-05-26 16:21:09 -0700

    Update ProjectFinalCopy.py

--- ProjectFinalCopy.py
+++ ProjectFinalCopy.py
@@ -118,7 +118,7 @@ Data_merged["Level of Education"] = ( Data_merged["Level of Education"].astype("
 # Merged Dataset Print
 Data_merged
 
-## PART 2: BASELINE MODEL: The baseline model (constant model) redicts the mean of our target variable median income for every observation.
+## PART 2: BASELINE MODEL: The baseline model (constant model) predicts the mean of our target variable median income for every observation.
 # Constant Model MSE
 constant_prediction = Data_merged['Median'].mean()
 Data_merged['pred_constant'] = constant_prediction
@@ -131,3 +131,87 @@ rmse_constant = np.sqrt(mse_constant)
 
 print("MSE of the constant model: $", mse_constant)
 print("RMSE of the constant model: $", rmse_constant)
+
+# PART 3: SIMPLE LINEAR REGRESSION: Model the relationship between PE1 and median income by fitting a linear equation. We calculated the slope and interception of this regression line and visualized it using a scatterplot.
+
+def standard_units(x):
+    return (x - np.mean(x)) / np.std(x)
+
+def correlation(x, y):
+    return np.mean(standard_units(x) * standard_units(y))
+
+def slope(x, y):
+    return correlation(x, y) * np.std(y) / np.std(x)
+
+def intercept(x, y):
+    return np.mean(y) - slope(x, y) * np.mean(x)
+
+# x and y setting again for calculations
+x = Data_merged["PE1"]
+y = Data_merged["Median"]
+
+# Slope + Intercept
+m = slope(x, y)
+b = intercept(x, y)
+
+# Predicted Median
+Predicted_Median = b + m * x
+# Sklearn Verification
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
+model.fit(x.values.reshape(-1,1), y)
+
+print(model.intercept_)  # 37987.79390014965
+print(model.coef_)   # 1154.1923081
+
+# Scatterplot with Regression Line
+
+# Predicted median calc
+Predicted_Median = b + m * x
+plot_4c = plt.figure()
+axis_4c = plt.axes()
+axis_4c.scatter(x, y)
+axis_4c.plot(x, Predicted_Median)
+
+# plot labels
+axis_4c.set_xlabel("PE1")
+axis_4c.set_ylabel("Median Income")
+axis_4c.set_title("PE1 vs Median Income with Regression Line")
+
+plt.show()
+
+# Residual Plot
+
+Data_merged['Predicted_Median'] = Predicted_Median
+residuals = Data_merged['Median'] - Data_merged['Predicted_Median']
+
+plt.scatter(Data_merged['Predicted_Median'], residuals)
+plt.xlabel('Predicted Median Income')
+plt.ylabel('Residuals')
+plt.title('Residual Plot')
+plt.axhline(y=0, color='b', linestyle = '--')
+plt.show()
+
+# PART 4: TRAIN / TEST SPLIT: Evaluate how the model worked when using held-out data and fix any potential problems of overfitting.
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+x = Data_merged[['PE1']]
+y = Data_merged['Median']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# Check for 80/20 split
+x_train.shape, x_test.shape, y_train.shape, y_test.shape
+
+# Fit model
+model = LinearRegression()
+
+model.fit(x_train, y_train)
+
+y_pred_test = model.predict(x_test)
+rmse_test = np.sqrt(np.mean((y_test - y_pred_test) ** 2))
+print('Test RMSE:', rmse_test)
+
